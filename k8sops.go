11c0c9f36872e637df069fbdc07373066811f5d0
prahaladh <prahaladhhn96@gmail.com>
2024-09-18 16:08:03 +0530

    Create k8sops.go

---
+++ k8sops.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	// Parse flags for Kubernetes config
+	var kubeconfig *string
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// Build the config from the kubeconfig file
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Service name as a parameter
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide a service name as a parameter.")
+		os.Exit(1)
+	}
+	serviceName := os.Args[1]
+
+	// Get the pods in the same namespace as the service
+	namespace := "default" // Change to your namespace
+	pods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("app=%s", serviceName),
+	})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Determine overall status
+	overallStatus := "Running" // Default status
+
+	for _, pod := range pods.Items {
+		// Check for non-running pod phase
+		if pod.Status.Phase == v1.PodFailed {
+			overallStatus = "Failed"
+			break
+		} else if pod.Status.Phase == v1.PodPending {
+			overallStatus = "Pending"
+		}
+
+		// Check container statuses for restarts or crash loops
+		for _, containerStatus := range pod.Status.ContainerStatuses {
+			if containerStatus.RestartCount > 0 {
+				fmt.Printf("Pod %s has container %s that restarted %d times\n", pod.Name, containerStatus.Name, containerStatus.RestartCount)
+			}
+			// Check for CrashLoopBackOff
+			if containerStatus.State.Waiting != nil && containerStatus.State.Waiting.Reason == "CrashLoopBackOff" {
+				overallStatus = "CrashLoopBackOff"
+				break
+			}
+		}
+	}
+
+	fmt.Printf("Overall Pod Status for Service '%s': %s\n", serviceName, overallStatus)
+}
+
+// Helper function to get the home directory
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
+}
