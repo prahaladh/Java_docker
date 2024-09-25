c883632b66a53c892aa9f289ac987e6689e1327d
prahaladh <prahaladhhn96@gmail.com>
2024-09-25 17:27:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,25 +1,28 @@
 import subprocess
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
-# Function to run a shell script with parameters
-def run_shell_script_with_params(script_path, param):
+# Function to run a shell script with two parameters
+def run_shell_script_with_two_params(script_path, param1, param2):
     try:
-        # Run the shell script with the given parameter
-        result = subprocess.run([script_path, param], capture_output=True, text=True, check=True)
-        return f"Output from {param}: {result.stdout}"
+        # Run the shell script with two parameters
+        result = subprocess.run([script_path, param1, param2], capture_output=True, text=True, check=True)
+        return f"Output from {param1}, {param2}: {result.stdout}"
     except subprocess.CalledProcessError as e:
-        return f"Error occurred while running the script with {param}: {e}"
+        return f"Error occurred while running the script with {param1}, {param2}: {e}"
 
-# List of parameters to pass to the script
-params = ["param1", "param2", "param3", "param4", "param5"]
+# First parameter (constant)
+param1 = "constant_param"
+
+# List of changing second parameters
+params2 = ["param2_1", "param2_2", "param2_3", "param2_4", "param2_5"]
 
 # Path to your shell script
 script_path = "./your_script.sh"
 
-# Use ProcessPoolExecutor to run the script in parallel with different parameters
-with ProcessPoolExecutor(max_workers=len(params)) as executor:
-    # Submit multiple jobs with different parameters
-    futures = [executor.submit(run_shell_script_with_params, script_path, param) for param in params]
+# Use ProcessPoolExecutor to run the script in parallel with two parameters
+with ProcessPoolExecutor(max_workers=len(params2)) as executor:
+    # Submit multiple jobs with the same first parameter and varying second parameters
+    futures = [executor.submit(run_shell_script_with_two_params, script_path, param1, param2) for param2 in params2]
 
     # Collect results as they are completed
     for future in as_completed(futures):
